c4032e647b159a5443f1be48bdefc6a8b60ee6e5
Bhupendra Tiwari <75015882+bhupendratiwari10@users.noreply.github.com>
2021-06-30 13:02:12 +0530

    Add files via upload

---
+++ SSS_Home.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="en-us">
+
+<head>
+
+
+  <title>Shivesh Security Service</title>
+  <meta name="description" content="Security service in mp,security services">
+  <meta name="view port" content="width=device-width initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+  <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> -->
+  <!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script> -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="Style.css">
+  <link rel="stylesheet" href="SSS.css">
+
+</head>
+
+<body>
+  <nav class="navbar navbar-expand-sm bg-dark navbar-dark navbar-fixed-top">
+    <!-- Brand -->
+    <img class="navbar-brand" src="" alt="logo">
+
+    <!-- Links -->
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link active" href="#">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="aboutus.html" target=_blank>About Us</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="contactus.html" target=_blank>Contact Us</a>
+      </li>
+
+      <!-- Dropdown -->
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+          More
+        </a>
+        <div class="dropdown-menu">
+          <a class="dropdown-item" href="#">Services</a>
+          <a class="dropdown-item" href="#">Vision</a>
+          <a class="dropdown-item" href="#">Mission</a>
+        </div>
+      </li>
+    </ul>
+   
+    <ul class=" nav navbar-nav navbar-right">
+      <li><a href="register.php"><span class="login glyphicon glyphicon-user"></span> Sign Up</a></li>
+      <li><a href="login.php"><span class="login glyphicon glyphicon-log-in"></span> Login</a></li>
+   
+    </ul>
+  </nav>
+  <div class="container">
+    <h1> Shivesh Security Services </h1>
+    <div id="Carousel" class="carousel slide" data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li data-target="#Carousel" data-slide-to="0" class="active"></li>
+        <li data-target="#Carousel" data-slide-to="1"></li>
+        <li data-target="#Carousel" data-slide-to="2"></li>
+        <li data-target="#Carousel" data-slide-to="3"></li>
+      </ol>
+      <div class="carousel-inner">
+        <div class="item active">
+          <img
+            src="https://image.shutterstock.com/image-photo/portrait-young-security-guard-talking-600w-344631824.jpg"
+            alt="Los Angeles" style="width:100%;">
+          <div class="carousel-caption">
+            <h3> Good Things </h3>
+            <p> Good things make us positive </p>
+          </div>
+        </div>
+        <div class="item">
+          <img
+            src="https://image.shutterstock.com/image-photo/portrait-young-security-guard-wearing-600w-311146826.jpg"
+            alt="Chicago" style="width:100%;">
+          <div class="carousel-caption">
+            <h3> Bad Thing </h3>
+            <p> bad things is a way of depresion!</p>
+          </div>
+        </div>
+        <div class="item">
+          <img
+            src="https://image.shutterstock.com/image-photo/security-man-corridor-using-portable-600w-521063830.jpg"
+            alt="New York" style="width:100%;">
+          <div class="carousel-caption">
+            <h3>Things for medium</h3>
+            <p>We love the our family much more</p>
+          </div>
+        </div>
+        <div class="item">
+          <img
+            src="https://image.shutterstock.com/image-photo/security-man-corridor-using-portable-600w-521063830.jpg"
+            alt="New York" style="width:100%;">
+          <div class="carousel-caption">
+            <h3>Things for medium</h3>
+            <p>We love the our family much more</p>
+          </div>
+        </div>
+      </div>
+      <a class="left carousel-control" href="#Carousel" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="right carousel-control" href="#Carousel" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+    </div>
+  <div class="container1">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate debitis quidem aperiam earum nihil? Illum, quia, repellendus suscipit atque accusamus vel, ipsa obcaecati iure accusantium nulla nam. Facilis voluptatibus dolores, exercitationem at necessitatibus labore, dolorum eligendi nesciunt blanditiis dolorem eius culpa. Eligendi aliquam facere atque. Iste quibusdam amet velit aperiam!</div>
+  <div class="container1 container2">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis, odit molestiae earum, obcaecati ipsa quasi suscipit culpa modi architecto est enim delectus alias. Ratione sequi itaque, accusantium, minima quo officia nihil, alias reprehenderit ipsam veritatis vel. Tempore non doloremque quas repudiandae esse nam, porro quibusdam, fugit nobis provident labore reprehenderit!</div>
+    
+
+  
+</body>
+
+</html>
